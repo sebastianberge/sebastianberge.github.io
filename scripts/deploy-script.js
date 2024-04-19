47065d5ac31cc05a8b7af55c7bbaa7d2b8916fe0
Sebastian Berge <sebastian.berge@tietoevry.com>
2024-04-19 17:18:41 +0200

    Fix: Changed workflow

--- scripts/deploy-script.js
+++ scripts/deploy-script.js
@@ -1,8 +1,8 @@
 const ghpages = require("gh-pages");
 
-ghpages.publish("dist", {
+ghpages.publish("build", {
   branch: "gh-pages",
-  repo: "https://github.com/sebastianberge/sebastianberge.github.io.git", // Make sure this is your repository URL
+  repo: "https://github.com/sebastianberge/sebastianberge.github.io.git",
   user: {
     name: "Sebastian Berge",
     email: "sebastian@berge.im",
@@ -10,5 +10,5 @@ ghpages.publish("dist", {
   silent: false,
   dotfiles: true,
   message: "Deploy commit",
-  token: process.env.GH_PAT, // Use the environment variable here
+  token: process.env.GH_PAT,
 });

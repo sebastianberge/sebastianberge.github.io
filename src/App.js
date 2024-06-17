75263670d799f4deba94cbf51d26f146d5309809
Sebastian Berge <31240927+sebastianberge@users.noreply.github.com>
2024-06-17 14:08:28 +0200

    Feat: New design (#7)
    * style: added the base-structure for the website
    
    * Feat: Added Blog-functionality
    
    * Feat: Added CV-page
    
    * Chore: Added node_modules to gitignore
    
    * Chore: Delete node_modules directory
    
    * Style: Added devicons
    
    * Style: Adjusted icons
    
    * Feat: Remake of Blog, About Me and CV
    
    * Fix: Removed Projects-page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import AboutMe from "./pages/AboutMe";
 import Blog from "./pages/Blog";
-import Projects from "./pages/Projects";
 import PostDetail from "./pages/PostDetail";
 import CV from "./pages/CV";
 
@@ -15,7 +14,6 @@ function App() {
       <Routes>
         <Route path="/" element={<AboutMe />} />
         <Route path="/blog" element={<Blog />} />
-        <Route path="/projects" element={<Projects />} />
         <Route path="/blog/:postId" element={<PostDetail />} />
         <Route path="/cv" element={<CV />} />
       </Routes>

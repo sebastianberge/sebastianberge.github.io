0c49b312b498b7a29874d3c2f552b2a8212bca7a
sebastianberge <sebastianberge@icloud.com>
2025-07-06 20:35:10 +0200

    Changed fontsize

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,10 @@ import { Box, Typography } from "@mui/material";
 const Footer = () => {
   return (
     <Box mt={8} py={3} textAlign="center">
-      <Typography variant="subtitle1">
+      <Typography
+        variant="subtitle1"
+        sx={{ fontWeight: 300, fontSize: "1rem" }}
+      >
         © {new Date().getFullYear()} Sebastian Berge
       </Typography>
     </Box>
